8fe4371b833c086545b08f8328a567cbe45e9cdb
KOndra96 <o.krechnak@seznam.cz>
2025-02-07 15:50:38 +0100

    script finished

--- js/script.js
+++ js/script.js
@@ -1,33 +1,49 @@
 const cardContainer = document.querySelector('.card');
 const confirmationContainer = document.querySelector('.confirmation');
 
-const cardButton = document.getElementById('card__button');
+const form = document.getElementById('card__form');
 const confirmationButton = document.getElementById('confirmation__button');
 
-cardButton.addEventListener('click', function(event) {
+form.addEventListener('submit', function(event) {
     event.preventDefault();
     
+    const data = {};
+    const fields = event['target'].querySelectorAll("input, select, textarea");
 
+    for(const field of fields) {
+        data[field.name] = field.value;
+    }
 
-    // inputError('email');
-    toggleConfirmation();
+    let inputEmail = data['email'];
+
+    if(!isEmail(inputEmail)) {
+        inputError('email');
+    } else {        
+        toggleConfirmation(inputEmail);
+    }
 });
 
 confirmationButton.addEventListener('click', function(event) {
     event.preventDefault();
-
     toggleConfirmation();
-
 });
 
-function toggleConfirmation() {
+function toggleConfirmation(email = 'ash@loremcompany.com') {
+
+    if(confirmationContainer.classList.contains('hide')) {
+        let confirmationText = document.getElementById('confirmation__text');
+        confirmationText.innerHTML = `
+            A confirmation email has been sent to <b>${email}</b>.
+            Please open it and click the button inside to confirm your subscription.
+        `;
+    }
+    
     cardContainer.classList.toggle('hide');
-    confirmationContainer.classList.toggle('hide');
+    confirmationContainer.classList.toggle('hide'); 
 }
 
 function inputError(object, message = "") {
     let errorInputObject = document.getElementById(object);
-
     let errorMessage = document.querySelector(`.${object}__error`);
 
     errorInputObject.classList.remove('input__ready');
@@ -38,3 +54,10 @@ function inputError(object, message = "") {
     }
     errorMessage.classList.remove('hide');
 }
+
+function isEmail(string) {
+    const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return pattern.test(string);
+}
+    
+    
